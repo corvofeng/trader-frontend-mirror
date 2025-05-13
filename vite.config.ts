4d24b34c14565d9b0a2d6be19d5dccf0d42c45f0
corvofeng <corvofeng@gmail.com>
2025-05-13 22:52:16 +0800

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,20 +4,20 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    allowedHosts: [
-      "stock.in.corvo.fun",
-    ],
-    proxy: {
-      '/api':  {
-        target: "http://127.0.0.1:8000/",
-        changeOrigin: true,
-      },
-      '/login':  {
-        target: "http://127.0.0.1:8000/",
-        changeOrigin: true,
-      },
-    },
-  },
+  //   allowedHosts: [
+  //     "stock.in.corvo.fun",
+  //   ],
+  //   proxy: {
+  //     '/api':  {
+  //       target: "http://127.0.0.1:8000/",
+  //       changeOrigin: true,
+  //     },
+  //     '/login':  {
+  //       target: "http://127.0.0.1:8000/",
+  //       changeOrigin: true,
+  //     },
+  //   },
+  // },
   
   plugins: [react()],
   optimizeDeps: {
