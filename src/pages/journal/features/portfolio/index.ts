dd889edf6dd75ad9754f9160c6a583e56825a4dd
corvofeng <corvofeng@gmail.com>
2025-08-23 09:30:57 +0000

    移除未使用的组件

--- src/pages/journal/features/portfolio/index.ts
+++ src/pages/journal/features/portfolio/index.ts
@@ -1,6 +1,1 @@
 export { Portfolio } from './Portfolio';
-export { PortfolioSummary } from './components/PortfolioSummary';
-export { HoldingsTable } from './components/HoldingsTable';
-export { PortfolioPieChart } from './components/PortfolioPieChart';
-export { RecentTradesTable } from './components/RecentTradesTable';
-export { DateRangeSelector } from './components/DateRangeSelector';
