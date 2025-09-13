e5984701f215e18155b505e61d5d0612f5f220bb
corvofeng <corvofeng@gmail.com>
2025-09-13 10:25:55 +0800

    Updated OptionsPortfolioManagement.tsx

--- src/features/options/components/OptionsPortfolioManagement.tsx
+++ src/features/options/components/OptionsPortfolioManagement.tsx
@@ -133,7 +133,7 @@ const convertPositionToStrategyLeg = (position: OptionsPosition): OptionStrategy
     contract_type_zh: position.contract_type_zh || '',
     contract_strike_price: position.strike,
     position_type: position.position_type,
-    position_type_zh: getPositionTypeZh(position.position_type, position.type),
+    position_type_zh: position.position_type_zh || getPositionTypeZh(position.position_type, position.type),
     leg_quantity: position.leg_quantity || position.selectedQuantity || position.quantity,
     cost_price: position.premium
   };
