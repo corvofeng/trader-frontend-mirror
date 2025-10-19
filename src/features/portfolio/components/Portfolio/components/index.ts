5ad3e935e0d777b7d0e39966a921a0737b3429d9
corvofeng <corvofeng@gmail.com>
2025-10-19 17:12:33 +0800

    Remove unused files.
    Signed-off-by: corvofeng <corvofeng@gmail.com>

--- src/features/portfolio/components/Portfolio/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { PortfolioHeader } from './PortfolioHeader';
-export { PortfolioMetrics } from './PortfolioMetrics';
-export { PortfolioHoldings } from './PortfolioHoldings';
-export { PortfolioTrades } from './PortfolioTrades';
